182cc194570da4dfd6cc488de537ec21e60ddee2
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-08-06 23:13:27 -0400

    added icon and hrline

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'; 
+
 const Main = () => {
     const navigate = useNavigate();
   
@@ -19,19 +22,20 @@ const Main = () => {
             </button>
           </div>
         </header>
-        <main className="container mx-auto py-8 flex-1">
-          <section>
-            <div className="flex items-center mb-8 mt-8">
-              <div className="bg-white-500 font-bold tracking-wide text-black p-4">Personal Info</div>
+        <main className="p-4 border-4 container mx-auto py-8 flex-1">
+            <div className="flex items-center mt-8">
+                <FontAwesomeIcon icon={faCircle} style={{color: "#88adec",}} />
+
+                <div className="bg-white-500 font-bold tracking-wide text-black p-4">Personal Info</div>
             </div>
+            <div className="hrLine"></div>
             <div className="bg-blue-200 text-black p-4">
-              <p>
-                Hello, this is Sean. I'm open to home/office nail
-                appointment. Please set up the appointment using the link
-                below. Address will be provided after you signed up.
-              </p>
+                <p>
+                    Hello, this is Sean. I'm open to home/office nail
+                    appointment. Please set up the appointment using the link
+                    below. Address will be provided after you signed up.
+                </p>
             </div>
-          </section>
         </main>
         <footer className="bg-blue-500 text-white p-4">
           © {new Date().getFullYear()} Your Company Name
