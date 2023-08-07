3442af6a216f0028f4af91538c641482ea46881a
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-08-06 22:49:06 -0400

    Used TailWind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,29 +6,6 @@ import React from "react";
 class App extends React.Component<any,any>{
   constructor(props:any){
     super(props);
-    this.state = {EventID: -1, arr:[]};
-    this.getEventID = this.getEventID.bind(this);
-    this.setEventID = this.setEventID.bind(this);
-    this.setarr = this.setarr.bind(this);
-    this.getarr = this.getarr.bind(this);
-
-  }
-  setEventID(id:number){
-    this.setState({EventID: id});
-  }
-  getEventID(){
-    return this.state.EventID;
-  }
-
-  setarr(arr:any[]){
-    let tmp:any[] = [];
-    for(let i in arr){
-      tmp.push(arr[i]);
-    }
-    this.setState({arr:tmp});
-  }
-  getarr(){
-    return this.state.arr;
   }
 
   render() {
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,34 +1,44 @@
-import { useNavigate } from 'react-router-dom';
-import "./Main.css";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+const Main = () => {
+    const navigate = useNavigate();
   
-class Main extends React.Component<any,any>{
-
-    constructor(props:any){
-        super(props);
-        this.state = {};
-        this.projectDisplay = this.projectDisplay.bind(this);
-
-    }
-
-    projectDisplay = ()=>{
-        this.props.navigate("./projectDisplay")
-    }
-//   const navigate = useNavigate();
+    const mainNavigation = () => {
+      navigate("/mainNavigation");
+    };
   
-  render(){
-        return (
-        <div className="App">
-        <header className="App-header">
-            <div className="topnav">
-                <button className="topnavButton" onClick={this.projectDisplay}>首页</button>
-                <button className="topnavButton" onClick={this.projectDisplay}>菜单</button>
-                <button className="topnavButton" onClick={this.projectDisplay}>成品展示</button>
-            </div>
+    return (
+      <div className="min-h-screen flex flex-col">
+        <header>
+          <div className="topnav">
+            <button
+              className="topnavButton"
+              onClick={mainNavigation}
+            >
+              首页
+            </button>
+          </div>
         </header>
-        </div>
-        );
-    }
-}
+        <main className="container mx-auto py-8 flex-1">
+          <section>
+            <div className="flex items-center mb-8 mt-8">
+              <div className="bg-white-500 font-bold tracking-wide text-black p-4">Personal Info</div>
+            </div>
+            <div className="bg-blue-200 text-black p-4">
+              <p>
+                Hello, this is Sean. I'm open to home/office nail
+                appointment. Please set up the appointment using the link
+                below. Address will be provided after you signed up.
+              </p>
+            </div>
+          </section>
+        </main>
+        <footer className="bg-blue-500 text-white p-4">
+          © {new Date().getFullYear()} Your Company Name
+        </footer>
+      </div>
+    );
+  };
   
+
 export default Main;
