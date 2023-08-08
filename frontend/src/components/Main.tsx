6bbc3f65cf22f91d62e5ce9de1e6cf2b3352982d
mengzhuou <71995520+mengzhuou@users.noreply.github.com>
2023-08-07 21:41:09 -0400

    css for personal contact

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,9 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'; 
+import { faCircle, faEnvelope, faMapMarkerAlt, faUser } from '@fortawesome/free-solid-svg-icons'; 
 import { useRef, useState } from 'react';
-import wechat from "./SeanWechat.jpg";
-import instagram from "./SeanInstagram.jpg";
+import wechat from "./XuanWechat.jpg";
+import instagram from "./XuanInstagram.jpg";
+import wechatIcon from "./wechat_icon.png";
+import instagramIcon from "./instagram_icon.png";
+import mainPic from "./MainPic.jpg";
+
 
 const Main = () => {
     const navigate = useNavigate();
@@ -36,41 +40,45 @@ const Main = () => {
         </header>
         <main className="p-4 border-4 container mx-auto py-8 flex-1">
             <div className="flex items-center mt-8">
-                <FontAwesomeIcon icon={faCircle} style={{color: "#88adec",}} />
+                <FontAwesomeIcon icon={faCircle} style={{color: "#88adec"}} />
                 <div className="bg-white-500 font-bold tracking-wide text-black p-4">Personal Info</div>
             </div>
             <div className="hrLine"></div>
-            <div className="flex items-center mt-8">
-                <ul>
-                    <li>
+            <div className="flex mb-10 p-4 container flex-1 flex justify-center">
+                <div className="w-100 h-100 ml-20 mr-20">
+                    <img src={mainPic} alt="mainPic"/>
+                </div>
+                <div className="flex-col items-center mt-8">
+                    <div className="flex">
+                        <img src={wechatIcon} alt="wechat" className="w-11 h-8 mt-3.5"/>
+                        <img src={instagramIcon} alt="instagram" className="w-11 ml-4 mt-2"/>
+                    </div>
+                    <div className="mt-3 text-lg">
+                        <FontAwesomeIcon icon={faUser} style={{color: "#19438a"}} />
+                        <span className="sr-only">Name:</span> Jerry Xuan
+
+                    </div>
+                    <div className="mt-1 text-lg">
                         <FontAwesomeIcon icon={faMapMarkerAlt} style={{color: "#19438a"}} />
-                        <span className="sr-only">Location:</span> Dallas, TX</li>
-                    <li>
+                        <span className="sr-only">Location:</span> Dallas, TX
+                    </div>
+                    <div className="mt-1 text-lg">
                         <FontAwesomeIcon icon={faEnvelope} style={{color: "#19438a"}} />
                         <span className="sr-only">Email:</span>
                         <a href="#" onClick={copyEmail} ref={emailRef} className="ml-1">dachenxuan@gmail.com</a>
-
                         {copied && <span style={{ marginLeft: "10px", color: "green" }}>Copied</span>}
-                    </li>
-                </ul>
-                <p>WeChat</p>
-                <img src={wechat} alt="wechat" className="w-1/5"/>
-                <p>Instagram</p>
-                <img src={instagram} alt="instagram" className="w-1/5"/>
-
+                    </div>
+                </div>
             </div>
             <div className="hrLine"></div>
             <div className="bg-blue-200 text-black p-4">
                 <p>
-                    Hello, this is Sean. I'm open to home/office nail
+                    Hello, I'm open to home/office nail
                     appointment. Please set up the appointment using the link
                     below. Address will be provided after you signed up.
                 </p>
             </div>
         </main>
-        <footer className="bg-blue-500 text-white p-4">
-          © {new Date().getFullYear()} Your Company Name
-        </footer>
       </div>
     );
   };
