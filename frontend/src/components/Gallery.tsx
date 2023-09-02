7567fcf21ac4fd0fa9ba19083351b5690a062387
mengzhuou <mou30@gatech.edu>
2023-09-02 00:32:16 -0400

    added carousel

--- frontend/src/components/Gallery.tsx
+++ frontend/src/components/Gallery.tsx
@@ -1,20 +1,40 @@
-import nail1 from "./nail1.jpg";
-import nail2 from "./nail2.jpg";
-import nail3 from "./nail3.jpg";
-import TopNavBar from './TopNavBar'; 
+import React from "react";
+import Carousel from "nuka-carousel";
+import nail1 from "./images/nail1.jpg";
+import nail2 from "./images/nail2.jpg";
+import nail3 from "./images/nail3.jpg";
+import nail4 from "./images/nail4.jpg";
+import TopNavBar from "./TopNavBar";
+import "./Gallery.css";
 
 const Gallery = () => {
-    return (
-      <div className="min-h-screen flex flex-col">
-        <TopNavBar/>
-        <main>
-            <img src={nail1} alt="nail1"/>
+  return (
+    <div className="min-h-screen flex flex-col">
+      <TopNavBar />
+      <main className="p-4 mx-auto flex-1">
+      <Carousel
+        wrapAround={true}
+        autoplay={true}
+        autoplayInterval={5000}
+        className="carousel-container"
+      >
+        <div className="carousel-slide">
+          <img src={nail1} alt="nail1" />
+        </div>
+        <div className="carousel-slide">
+          <img src={nail2} alt="nail2" />
+        </div>
+        <div className="carousel-slide">
+          <img src={nail3} alt="nail3" />
+        </div>
+        <div className="carousel-slide">
+          <img src={nail4} alt="nail4" />
+        </div>
+      </Carousel>
 
-        </main>
-        
-      </div>
-    );
-  };
-  
+      </main>
+    </div>
+  );
+};
 
 export default Gallery;
